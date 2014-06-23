093720197bdba2290789bffe78bf9ac644f45fce
showa_yojyo <yojyo@hotmail.com>
2014-06-24 00:37:22 +0900

    (Constants.cs) new file.

---
+++ WestWorld/Constants.cs
@@ -0,0 +1,28 @@
+﻿// Original author:
+//   Programming Game AI by Example, Mat Buckland, 2002.
+//   (http://www.jblearning.com/catalog/9781556220784/)
+
+namespace WestWorld
+{
+    /// <summary>
+    /// From Location.h
+    /// </summary>
+    public enum LocationType
+    {
+        Shack,
+        Goldmine,
+        Bank,
+        Saloon,
+    };
+
+    /// <summary>
+    /// From EntityNames.h
+    /// </summary>
+    public enum EntityType
+    {
+        MinerBob,
+        Elsa,
+    };
+
+    // Function GetNameOfEntity is not needed in C# (use EntityType.ToString).
+}
