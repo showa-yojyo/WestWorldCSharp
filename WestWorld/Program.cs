65d5a912ae4b3e50ef50b0a744f6a399023f0406
showa_yojyo <yojyo@hotmail.com>
2014-06-23 23:53:14 +0900

    New project.

---
+++ WestWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// Original author:
+//   Programming Game AI by Example, Mat Buckland, 2002.
+//   (http://www.jblearning.com/catalog/9781556220784/)
+
+using System;
+
+namespace WestWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ WestWorld/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WestWorld")]
+[assembly: AssemblyDescription("From \"Programming Game AI by Example\" Chapter 2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WestWorld")]
+[assembly: AssemblyCopyright("Copyright © 2002 Mat Buckland")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0394aba1-845c-4e22-bbb7-c0a30634495d")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
