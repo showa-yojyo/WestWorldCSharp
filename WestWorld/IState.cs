c6fe75a1b555c99c94f13878687624d2c97769cf
showa_yojyo <yojyo@hotmail.com>
2014-06-24 23:49:54 +0900

    (IState.cs) new file.

---
+++ WestWorld/IState.cs
@@ -0,0 +1,30 @@
+﻿// Original author:
+//   Programming Game AI by Example, Mat Buckland, 2002.
+//   (http://www.jblearning.com/catalog/9781556220784/)
+
+namespace WestWorld
+{
+    /// <summary>
+    /// Interface to define an interface for a state.
+    /// </summary>
+    public interface IState
+    {
+        /// <summary>
+        /// This will execute when the state is entered.
+        /// </summary>
+        /// <param name="miner"></param>
+        void Enter(Miner miner);
+
+        /// <summary>
+        /// This is the state's normal update function.
+        /// </summary>
+        /// <param name="miner"></param>
+        void Execute(Miner miner);
+
+        /// <summary>
+        /// This will execute when the state is exited.
+        /// </summary>
+        /// <param name="miner"></param>
+        void Exit(Miner miner);
+    }
+}
